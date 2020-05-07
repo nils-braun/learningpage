458f7e35cb4f07a604adde9ec455fc454844c6e3
Nils Braun <nilslennartbraun@gmail.com>
2020-05-07 21:14:48 +0200

    Rename to learningpage

--- backend/jupyterhub_config.py
+++ backend/jupyterhub_config.py
@@ -3,7 +3,7 @@ import sys
 
 c.JupyterHub.services = [
     {
-        "name": "learnpage",
+        "name": "learningpage",
         "url": "http://127.0.0.1:10101",
         "command": ["flask", "run", "--port=10101"],
         "environment": {"FLASK_APP": "app.py", "FLASK_DEBUG": "1"},
