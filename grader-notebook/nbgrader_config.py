96c26ada2a96933bb5ac701d24c8c48e6c0d5244
Nils Braun <nilslennartbraun@gmail.com>
2020-05-10 19:40:34 +0200

    Exchange is not needed anymore

--- grader-notebook/nbgrader_config.py
+++ grader-notebook/nbgrader_config.py
@@ -4,5 +4,4 @@ c = get_config()
 
 c.CourseDirectory.course_id = "course"
 c.CourseDirectory.root = "/home/jovyan/content"
-c.Exchange.root = "/home/jovyan/exchange"
 c.CourseDirectory.db_url = os.environ.get('GRADER_DATABASE_STRING')
