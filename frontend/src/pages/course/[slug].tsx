9b9e30f6852c06a51ec0fc584df1aec85100d357
dotcs <git@dotcs.me>
2020-05-21 12:24:47 +0200

    Improve typings

--- frontend/src/pages/course/[slug].tsx
+++ frontend/src/pages/course/[slug].tsx
@@ -1,14 +1,15 @@
-import CoursePage from '../../components/CoursePage';
+import CoursePage, { CoursePageProps } from '../../components/CoursePage';
 import { apiBaseUrl } from '../../utils';
 import { ApiCoursePage } from '../../types';
+import { GetServerSideProps } from 'next';
 
-export async function getServerSideProps({ query }) {
-  const { slug } = query;
+export const getServerSideProps: GetServerSideProps<CoursePageProps> = async context => {
+  const { slug } = context.query;
   const url = [apiBaseUrl, 'content', slug].join('/');
-  const content: ApiCoursePage = await fetch(url, { method: 'GET' })
+  const page: ApiCoursePage = await fetch(url, { method: 'GET' })
     .then(res => res.json())
     .catch(err => console.error(err));
-  return { props: { content } }
+  return { props: { page } }
 }
 
 export default CoursePage;
