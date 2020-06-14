c8126d2e068896a26aa2a6ca725a6d84253947b8
dotcs <git@dotcs.me>
2020-06-14 21:39:29 +0200

    Simplify code with useSelector. How awesome is this?!

--- frontend/src/components/CoursePage/index.tsx
+++ frontend/src/components/CoursePage/index.tsx
@@ -6,44 +6,37 @@ import AboutSection from './subcomponents/AboutSection';
 import InstructorsSection from './subcomponents/InstructorsSection';
 import AnchorRow from './subcomponents/AnchorRow';
 import { getContentStartLink } from '../../utils';
-import { connect, ConnectedProps } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-export interface CoursePageOwnProps {
+export interface CoursePageProps {
     page: CoursePageContent;
 }
 
-const mapState = (state: RootState) => ({
-    userIsAnonymous: state.user.isAnonymous
-});
-const mapDispatch = {}
+const CoursePage: SFC<CoursePageProps> = (props) => {
+    const user = useSelector((state: RootState) => state.user);
+    return (
+        <>
+            <Header
+                slug={props.page.slug}
+                title={props.page.title}
+                subtitle={props.page.subtitle}
+                hasAssignment={props.page.hasAssignment}
+                allowSubmit={!user.isAnonymous}
+                startLink={getContentStartLink(props.page.slug)}
+            />
+            <AnchorRow />
+            <AboutSection
+                description={props.page.description}
+                learnings={props.page.learnings}
+                skills={props.page.skills}
+                facts={props.page.facts.map(f => ({ type: f.key, value: f.value, extra: f.extra }))}
+                level={props.page.level}
+            />
+            <InstructorsSection
+                instructors={props.page.instructors}
+            />
+        </>
+    );
+};
 
-const connector = connect(mapState, mapDispatch);
-type CoursePagePropsFromRedux = ConnectedProps<typeof connector>;
-
-export type CoursePageProps = CoursePagePropsFromRedux & CoursePageOwnProps;
-
-const CoursePage: SFC<CoursePageProps> = (props) => (
-    <>
-        <Header
-            slug={props.page.slug}
-            title={props.page.title}
-            subtitle={props.page.subtitle}
-            hasAssignment={props.page.hasAssignment}
-            allowSubmit={!props.userIsAnonymous}
-            startLink={getContentStartLink(props.page.slug)}
-        />
-        <AnchorRow />
-        <AboutSection
-            description={props.page.description}
-            learnings={props.page.learnings}
-            skills={props.page.skills}
-            facts={props.page.facts.map(f => ({ type: f.key, value: f.value, extra: f.extra }))}
-            level={props.page.level}
-        />
-        <InstructorsSection
-            instructors={props.page.instructors}
-        />
-    </>
-);
-
-export default connector(CoursePage);
+export default CoursePage;
