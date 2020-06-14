a2569f9e80722e82ad264b9a35e09402a238006d
dotcs <git@dotcs.me>
2020-06-14 20:10:27 +0200

    Refactor code

--- frontend/src/components/CoursePage/index.tsx
+++ frontend/src/components/CoursePage/index.tsx
@@ -12,29 +12,27 @@ export interface CoursePageProps {
     page: CoursePageContent;
 }
 
-const CoursePage: SFC<CoursePageProps> = ({ page }) => {
-    return (
-        <>
-            <Header
-                slug={page.slug}
-                title={page.title}
-                subtitle={page.subtitle}
-                hasAssignment={page.hasAssignment}
-                startLink={getContentStartLink(page.slug)}
-            />
-            <AnchorRow />
-            <AboutSection
-                description={page.description}
-                learnings={page.learnings}
-                skills={page.skills}
-                facts={page.facts.map(f => ({ type: f.key, value: f.value, extra: f.extra }))}
-                level={page.level}
-            />
-            <InstructorsSection
-                instructors={page.instructors}
-            />
-        </>
-    )
-};
+const CoursePage: SFC<CoursePageProps> = ({ page }) => (
+    <>
+        <Header
+            slug={page.slug}
+            title={page.title}
+            subtitle={page.subtitle}
+            hasAssignment={page.hasAssignment}
+            startLink={getContentStartLink(page.slug)}
+        />
+        <AnchorRow />
+        <AboutSection
+            description={page.description}
+            learnings={page.learnings}
+            skills={page.skills}
+            facts={page.facts.map(f => ({ type: f.key, value: f.value, extra: f.extra }))}
+            level={page.level}
+        />
+        <InstructorsSection
+            instructors={page.instructors}
+        />
+    </>
+);
 
 export default CoursePage;
--- frontend/src/components/CoursePage/subcomponents/Header.tsx
+++ frontend/src/components/CoursePage/subcomponents/Header.tsx
@@ -27,40 +27,38 @@ type HeaderPropsFromRedux = ConnectedProps<typeof connector>;
 
 export type HeaderProps = HeaderPropsFromRedux & HeaderOwnProps;
 
-const Header: SFC<HeaderProps> = (props) => {
-    return (
-        <div className="bg-blue-500 text-white">
-            <div className="container mx-auto py-6">
-                <div className="grid grid-cols-1 lg:grid-cols-3">
-                    <div className="lg:col-span-2 flex flex-col mb-4">
-                        <h1 className="text-4xl">{props.title}</h1>
-                        <div className="flex-grow">
-                            <p className="text-xl">{props.subtitle}</p>
-                        </div>
-                    </div>
-                    <div className="mb-4">
-                        Offered by:
-                        <img src="//via.placeholder.com/350x350" />
+const Header: SFC<HeaderProps> = (props) => (
+    <div className="bg-blue-500 text-white">
+        <div className="container mx-auto py-6">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+                <div className="lg:col-span-2 flex flex-col mb-4">
+                    <h1 className="text-4xl">{props.title}</h1>
+                    <div className="flex-grow">
+                        <p className="text-xl">{props.subtitle}</p>
                     </div>
                 </div>
-                <div>
-                    <div className="inline-block mr-8">
-                        <a href={props.startLink} className="btn btn-white">Start</a>
-                        <div>10.000 enrolled</div>
-                    </div>
-                    {props.hasAssignment && 
-                        <div className="inline-block">
-                            <button className="btn btn-white" 
-                                onClick={() => props.handleSubmit(props.slug)}
-                                disabled={props.submissionFetching}
-                            >Submit</button>
-                            <div>{props.submissionsCount} submissions</div>
-                        </div>
-                    }
+                <div className="mb-4">
+                    Offered by:
+                    <img src="//via.placeholder.com/350x350" />
                 </div>
             </div>
+            <div>
+                <div className="inline-block mr-8">
+                    <a href={props.startLink} className="btn btn-white">Start</a>
+                    <div>10.000 enrolled</div>
+                </div>
+                {props.hasAssignment && 
+                    <div className="inline-block">
+                        <button className="btn btn-white" 
+                            onClick={() => props.handleSubmit(props.slug)}
+                            disabled={props.submissionFetching}
+                        >Submit</button>
+                        <div>{props.submissionsCount} submissions</div>
+                    </div>
+                }
+            </div>
         </div>
-    );
-}
+    </div>
+);
 
 export default connector(Header);
